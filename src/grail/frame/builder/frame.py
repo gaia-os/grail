595c4ac19b5c80d993dcee1d02a8bf85af75030f
Michael Walters <walters.m93@gmail.com>
2026-08-29 21:54:38 +0200

    move builder into new frame module

---
+++ src/grail/frame/builder/frame.py
@@ -0,0 +1,5 @@
+"""Deprecated compatibility import for :mod:`grail.frame.frame`."""
+
+from grail.frame import Frame, FrameMetadata
+
+__all__ = ["Frame", "FrameMetadata"]
---
+++ src/grail/frame/builder/variable.py
@@ -0,0 +1,5 @@
+"""Deprecated compatibility import for :mod:`grail.frame.variable`."""
+
+from grail.frame import Variable
+
+__all__ = ["Variable"]
